dba4aa7e4cbfaf785b1133ade547c38ab932ceb9
GitHub Action <action@github.com>
2021-04-12 10:10:50 +0000

    Bump version to 0.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "management-cluster-admission"
 	source      = "https://github.com/giantswarm/management-cluster-admission"
-	version     = "0.1.1"
+	version     = "0.1.2-dev"
 )
 
 func Description() string {
