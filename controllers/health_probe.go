7741152bc095cc50490e45daef7769ef23185d41
kubasobon <wtty.fool@gmail.com>
2021-04-21 14:47:25 +0200

    add k8s API call timeout

--- controllers/health_probe.go
+++ controllers/health_probe.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/giantswarm/microerror"
 	appsv1 "k8s.io/api/apps/v1"
@@ -16,12 +17,16 @@ type HealthProbe struct {
 }
 
 func (v *HealthProbe) HealthzCheck(_ *http.Request) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	// Check whether manager's client is able to communicate with Kubernetes API.
 	kubeSystemDeployments := appsv1.DeploymentList{}
-	err := v.List(context.Background(), &kubeSystemDeployments, &client.ListOptions{
+	err := v.List(ctx, &kubeSystemDeployments, &client.ListOptions{
 		Namespace: "kube-system",
 		Limit:     1,
 	})
+
 	if err != nil {
 		return microerror.Mask(err)
 	}
